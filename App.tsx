9f2bea976c6a00aa3a0e4fa2f153abfee78df759
PlayNProvide <playnprovide@gmail.com>
2020-12-29 22:55:18 +0900

    unhandled promise rejection

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState, useRef} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 import NativeAdView, {
   CallToActionView,
